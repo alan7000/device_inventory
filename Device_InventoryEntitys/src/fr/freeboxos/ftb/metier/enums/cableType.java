ca30dd65927abd7d815dd502050bceef13a13366
Alan700 <ftbmodified700@gmail.com>
2019-05-04 22:33:40 +0200

    Add Enum

---
+++ Device_InventoryEntitys/src/fr/freeboxos/ftb/metier/enums/cableType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.freeboxos.ftb.metier.enums;
+
+/**
+ *
+ * @author alan
+ */
+public enum cableType {
+    Audio, Vidéo, Adaptateur_RCA_Cinch, Audio_et_Vidéo, Audio_mic, Alimentation_DS, Alimentation_USB, Adaptateur, Cable_USB;
+}
